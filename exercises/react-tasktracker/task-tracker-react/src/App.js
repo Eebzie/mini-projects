8ddcb63215dc13b72c41298b621e8beff94c50ca
Eebzie <gabrielle.agustin@gmail.com>
2021-02-26 02:47:04 -0500

    First React task tracker

---
+++ exercises/react-tasktracker/task-tracker-react/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  const name = 'Gabby'
+  return (
+    <div className="container">
+      <h1>Hello From React</h1>
+      <h2>Hello {name}</h2>
+    </div>
+  );
+}
+
+export default App;

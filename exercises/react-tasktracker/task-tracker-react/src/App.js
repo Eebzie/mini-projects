dd4cfb13ef68aa883de7ecc0e09419fdc4c232f6
Eebzie <gabrielle.agustin@gmail.com>
2021-02-27 20:21:46 -0500

    General styling

--- exercises/react-tasktracker/task-tracker-react/src/App.js
+++ exercises/react-tasktracker/task-tracker-react/src/App.js
@@ -1,9 +1,9 @@
+import Header from './components/Header'
+
 function App() {
-  const name = 'Gabby'
   return (
     <div className="container">
-      <h1>Hello From React</h1>
-      <h2>Hello {name}</h2>
+      <Header/>
     </div>
   );
 }
